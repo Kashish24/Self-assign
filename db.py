8fc1603967e2b1cc55b304f4785e9168a3ac9600
kashishkhurana <kashishkhurana16@gmail.com>
2024-06-23 22:45:14 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,43 @@
+import duckdb
+
+class DuckDBConnection:
+    def __init__(self, database='warehouse.db'):
+        self.database = database
+        self.connection = None
+
+    def connect(self):
+        if self.connection is None:
+            self.connection = duckdb.connect(self.database)
+        return self.connection
+
+    def close(self):
+        if self.connection is not None:
+            self.connection.close()
+            self.connection = None
+
+    def execute_query(self, query, params=None):
+        if self.connection is None:
+            raise Exception("Connection is not established. Call connect() first.")
+        if params:
+            return self.connection.execute(query, params)
+        else:
+            return self.connection.execute(query)
+    
+    def fetchall(self, query, params=None):
+        if self.connection is None:
+            raise Exception("Connection is not established. Call connect() first.")
+        if params:
+            return self.connection.execute(query, params).fetchall()
+        else:
+            return self.connection.execute(query).fetchall()
+
+# Test function as provided in the sample code
+def test_duckdb_connection():
+    conn = DuckDBConnection()
+    conn.connect()
+    cursor = conn.connection
+    assert list(cursor.execute("SELECT 1").fetchall()) == [(1,)]
+    conn.close()
+
+if __name__ == "__main__":
+    test_duckdb_connection()
